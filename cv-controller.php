89bb71af149cbbf416b7025386c14943f499ef00
electricg <electric.g@gmail.com>
2013-08-17 14:39:46 +0100

    Implemented mustache template

---
+++ cv-controller.php
@@ -0,0 +1,38 @@
+<?php
+$template = 'cv-template';
+$json = 'data.json';
+
+date_default_timezone_set('Europe/London');
+
+require '../lib/php/mustache.php/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+// use html files as templates
+$options =  array('extension' => '.html');
+
+$mustache = new Mustache_Engine(array(
+	'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__), $options)
+));
+
+// format date
+$mustache->addHelper('month', function($value) { return date('F', strtotime($value)); });
+$mustache->addHelper('year',  function($value) { return date('Y', strtotime($value)); });
+
+$tpl = $mustache->loadTemplate($template);
+
+$string = file_get_contents($json);
+
+// format metadata available inside json data
+$data['today'] = date("Y-m-d");
+$pattern = '|({{@)([\w]+)(}})|';
+function replacement($matches) {
+    global $data;
+    return $data[$matches[2]];
+}
+$string = preg_replace_callback($pattern, 'replacement', $string);
+
+$json = json_decode($string, true);
+
+$cv = $tpl->render($json);
+echo $cv;
+?>
