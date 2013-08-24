1537bb0b7655963043961c22c231f4f55ee6c8e3
electricg <electric.g@gmail.com>
2013-08-24 21:53:31 +0100

    Template extensions as variable

--- cv-controller.php
+++ cv-controller.php
@@ -1,6 +1,7 @@
 <?php
 $template = 'cv-template';
 $json = 'data.json';
+$ext = '.html';
 
 date_default_timezone_set('Europe/London');
 
@@ -8,7 +9,7 @@ require '../lib/php/mustache.php/src/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
 // use html files as templates
-$options =  array('extension' => '.html');
+$options =  array('extension' => $ext);
 
 $mustache = new Mustache_Engine(array(
 	'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__), $options)
