fb738f82a5a05ab01a3705aa71dd9443422db5c3
electricg <electric.g@gmail.com>
2013-08-26 16:55:53 +0100

    Merged private creation of rtf and html into one single file

--- cv.php
+++ cv.php
@@ -1,19 +1,25 @@
 <?php
-// Avoid echo from public controller
-ob_start();
-
-include 'cv-controller.php';
-
-// End of avoid echo from public controller
-ob_end_clean();
-
 $data['telephone'] = '07411480415';
 $data['email'] = 'giulia.alfonsi@gmail.com';
 $data['street-address'] = '29A Vallance Gardens';
 $data['locality'] = 'Hove';
 $data['postal-code'] = 'BN3 2DB';
 
+$open = 'cv-controller.php';
 $pattern_private = '|({#)([\w\-]+)(#})|';
+
+// Print doc format
+if ($_GET['f'] == 'rtf') {
+	$open = 'doc-controller.php';
+	$pattern_private = '|(\\\{#)([\w\-]+)(#\\\})|';
+}
+// Avoid echo from public controller
+ob_start();
+
+include $open;
+
+// End of avoid echo from public controller
+ob_end_clean();
 
 $cv = preg_replace_callback($pattern_private, 'replacement', $cv);
 echo $cv;
