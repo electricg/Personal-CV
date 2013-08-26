7c4c86322336b7c31f5f4105c0e0dd22d9ae4c76
electricg <electric.g@gmail.com>
2013-08-26 17:10:09 +0100

    Merged controller for html and rtf in one single file

--- cv-controller.php
+++ cv-controller.php
@@ -1,7 +1,62 @@
 <?php
+$format = $_GET['f'];
+
 $template = 'cv-template';
 $json = 'data.json';
 $ext = '.html';
+$d = 'dHtml';
+
+// Html
+// decode newline characters
+function dHtml($arr) {
+	foreach ($arr as $key => $value) {
+		if (gettype($value) == 'array') {
+			$arr[$key] = dHtml($value);
+		}
+		elseif (gettype($value) == 'string') {
+			// inserts HTML line breaks before all newlines
+			$arr[$key] = nl2br($value, false);
+		}
+	}
+	return $arr;
+}
+
+// Rtf
+// decode characters and encode the special ones for rtf
+function dRtf($arr) {
+	foreach ($arr as $key => $value) {
+		if (gettype($value) == 'array') {
+			$arr[$key] = dRtf($value);
+		}
+		elseif (gettype($value) == 'string') {
+			$value = iconv('UTF-8', 'CP1256', $value);
+			$len = strlen($value);
+			$new_value = '';
+			for($i = 0; $i <= $len; $i++ ) {
+				$o = ord($value[$i]);
+				$v = $value[$i];
+				// escape characters over code 127
+				if ($o > 127) {
+					$v = "\'".dechex($o);
+				}
+				// escape { } \
+				if ($o == 123 || $o == 125 || $o == 92) {
+					$v = '\\'.$v;
+				}
+				$new_value .= $v;
+			}
+			// inserts rtf line breaks before all newlines
+			$new_value = str_replace("\n", "\\\n", $new_value);
+			$arr[$key] = $new_value;
+		}
+	}
+	return $arr;
+}
+
+if ($format == 'rtf') {
+	$ext = '.rtf';
+	$d = 'dRtf';
+}
 
 date_default_timezone_set('Europe/London');
 
@@ -34,20 +89,19 @@ $string = preg_replace_callback($pattern, 'replacement', $string);
 
 $json = json_decode($string, true);
 
-// decode newline characters
-function d($arr) {
-	foreach ($arr as $key => $value) {
-		if (gettype($value) == 'array') {
-			$arr[$key] = d($value);
-		}
-		elseif (gettype($value) == 'string') {
-			// inserts HTML line breaks before all newlines
-			$arr[$key] = nl2br($value, false);
-		}
-	}
-	return $arr;
+$json = $d($json);
+
+if ($format == 'rtf') {
+	// about section - last item - to avoid printing new line
+	$c = count($json['about']['items']);
+	$json['about']['items'][$c - 1]['last'] = true;
+	
+	$file_name = strtolower($json['person']['name'].$json['person']['surname']).'-cv'.$ext;
+
+	header('Content-Type: application/rtf');
+	header("Content-Transfer-Encoding: Binary"); 
+	header("Content-disposition: attachment; filename=\"".$file_name."\"");
 }
-$json = d($json);
 
 $cv = $tpl->render($json);
 echo $cv;
